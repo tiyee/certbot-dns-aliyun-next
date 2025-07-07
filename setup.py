763ee5b49ca7ff5dca7434788eaed346bf032aeb
tiyee <tiyee@live.com>
2025-07-07 15:03:03 +0800

    release-1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="certbot-dns-aliyun-next",
-    version="1.0.0",
+    version="1.0.1",
     author="tiyee",
     author_email="tiyee@live.com",
     description="阿里云DNS插件，用于Certbot的DNS-01验证",
