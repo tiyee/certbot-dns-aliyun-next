2874e738fe674a88a3ee9e41fdfb3c2371594baf
tiyee <tiyee@live.com>
2025-07-30 17:19:28 +0800

    v1.0.5

--- setup.py
+++ setup.py
@@ -14,13 +14,18 @@ setup(
     description="阿里云DNS插件，用于Certbot的DNS-01验证",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="certbot-dns certbot-plugin certbot-dns-plugin certbot-dns-authenticator certbot-aliyun",
     url="https://github.com/tiyee/certbot-dns-aliyun-next",
+    project_urls={
+        "Homepage": "https://github.com/tiyee/certbot-dns-aliyun-next"
+    },
     packages=find_packages(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Plugins",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: Apache Software License",
+        'Operating System :: POSIX',
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
